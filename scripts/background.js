3d1dd88f3c149e1e28e2573b8982ae251f9708db
Keren Meltzer <keren@basistech.com>
2014-07-31 22:26:55 +0300

    chrome-app show granny's pic and then run presentation

---
+++ scripts/background.js
@@ -0,0 +1,18 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  
+  chrome.app.window.create('index.html', {
+    id: "mainAppWindowID",
+    bounds: {
+      width: screen.availWidth,
+      height: screen.availHeight,
+      left: 0,
+      top: 0
+    }
+  });
+});
